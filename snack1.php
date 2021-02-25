a36afc03a27ec4887f6a0c2ac8a564a1b022ab49
Fede <chiellofederico@gmail.com>
2021-02-25 14:30:09 +0100

    Init

---
+++ snack1.php
@@ -0,0 +1,13 @@
+<!-- Creiamo un array 'matches' contenente altri array i quali rappresentano delle partite di basket di un’ipotetica tappa del calendario. Ogni array della partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema: Olimpia Milano - Cantù | 55 - 60 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Array 'matches'</title>
+</head>
+<body>
+
+</body>
+</html>
